4ef00e9d13645fd650e3fc1552fb82f7ca4521f3
Dominic Lee <dominictwlee@gmail.com>
2020-11-06 15:20:14 +0000

    add additional templates

---
+++ templates/react/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export function Sample() {
+  return <div>Replace me</div>;
+}
---
+++ templates/typescriptreact/src/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export function Sample(props) {
+  return <div>Replace me</div>;
+}
